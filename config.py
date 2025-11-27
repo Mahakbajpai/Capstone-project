fd88daeb8623d615178af3ebb0851011c973cd3b
Mahakbajpai <mahakbajpai77@gmail.com>
2025-11-28 02:36:30 +0530

    Fix GOOGLE_API_KEY assignment and check condition

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 # GOOGLE GEMINI API KEY
 # Get this from https://aistudio.google.com/
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "AIzaSyCGX3-daUblGSZJdTrboskrw5GoEnNnqtw")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "GOOGLE_API_KEY=AIzaSyCGX3-daUblGSZJdTrboskrw5GoEnNnqtw")
 
-if GOOGLE_API_KEY == "AIzaSyCGX3-daUblGSZJdTrboskrw5GoEnNnqtw":
+if GOOGLE_API_KEY == "GOOGLE_API_KEY=AIzaSyCGX3-daUblGSZJdTrboskrw5GoEnNnqtw":
     print("WARNING: You need to set your Google API Key in config.py")
